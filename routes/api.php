620f3120fdb986667412f0345376ff409c0f6dc5
joehunt588 <johan_newdestiny@yahoo.com.my>
2023-09-19 00:06:56 +0800

    create roles and permission for video 18

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -41,4 +43,6 @@ Route::middleware('auth:sanctum')->group(function () {
     // Route::delete('users/{id}', [UserController::class, 'destroy']);
     // another methods for user controller
     Route::apiResource('users',UserController::class);
+    Route::apiResource('roles',RoleController::class);
+    Route::get('permissions',[PermissionController::class, 'index']);
 });
